235d1dea484b76bc8822ba97b9f76aa1a2fa8abc
Natalia Szlapka <n.machalica@gmail.com>
2021-08-23 16:42:54 +0200

    Add identification

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+
 const socket = require('socket.io');
 const app = express();
 
-const tasks = [];
-
 app.use(cors());
+
+const tasks = [];
 
 const server = app.listen(process.env.PORT || 8000, () => {
 	console.log('Server is running on port 8000')
 });
 
-const io = socket(server);
+const io = socket(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST']
+	}
+});
 
 app.use((req, res) => {
 	res.status(404).send('404 not found')
 });
 
 io.on('connection', (socket) => {
+
+	console.log('New client! Id:' + socket.id);
+
+	socket.on('addTask', newTask => {
+		console.log('New task added ', newTask, 'by user: ' + socket.id);
+    tasks.push(newTask);
+		socket.broadcast.emit('addTask', newTask);
+	});
+
+	socket.on('removeTask', (id) => {
+		console.log('Task removed', id);
+		task.splice(id, 1);
+		socket.broadcast.emit('removeTask', id);
+	});
+
 	socket.emit('updateData', tasks);
-
-	socket.on('addTask', task => {
-		console.log(`${socket.id} added ${task}`);
-    tasks.push(task);
-		socket.broadcast.emit('addTask', task);
-	});
-
-	socket.on('removeTask', i => {
-		console.log(`${socket.id} removed task ${i}`);
-		task.splice(i, 1);
-		socket.broadcast.emit('removeTask', i);
-	});
 });
